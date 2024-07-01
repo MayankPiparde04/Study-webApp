3fb9af147cc766dcead53aa049139d0b457452cc
Mayank Piparde <mayankpiparde20@gmail.com>
2024-07-02 00:43:33 +0530

    Day 3.1

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,56 +1,22 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import data from '../../db.json';
 
-const home = () => {
+const Home = () => {
+  const { chapters } = data;
+
   return (
-    <View>
-      <TouchableOpacity>
-        <View className='h-24 mt-2 mx-2 justify-center items-center border bg-gradient-to-r bg-'
-          style={[styles.lightGreen]}>
-          <Text className='font-semibold text-2xl'>Chapter 1</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <View className='h-24 mt-2 mx-2 justify-center items-center border bg-gradient-to-r bg-'
-          style={[styles.lightOrange]}>
-          <Text className='font-semibold text-2xl'>Chapter 2</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <View className='h-24 mt-2 mx-2 justify-center items-center border bg-gradient-to-r bg-'
-          style={[styles.lightGreen]}>
-          <Text className='font-semibold text-2xl'>Chapter 3</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <View className='h-24 mt-2 mx-2 justify-center items-center border bg-gradient-to-r bg-'
-          style={[styles.lightOrange]}>
-          <Text className='font-semibold text-2xl'>Chapter 4</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <View className='h-24 mt-2 mx-2 justify-center items-center border bg-gradient-to-r bg-'
-          style={[styles.lightGreen]}>
-          <Text className='font-semibold text-2xl'>Chapter 5</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <View className='h-24 mt-2 mx-2 justify-center items-center border bg-gradient-to-r bg-'
-          style={[styles.lightOrange]}>
-          <Text className='font-semibold text-2xl'>Chapter 6</Text>
-        </View>
-      </TouchableOpacity>
+    <View className='flex-1 mx-2 my-2'>
+      {chapters.map((chapter) => (
+        <TouchableOpacity>
+          <View key={chapter.id} 
+          className=' bg-sky-400 h-20 py-2 my-2 justify-center items-center rounded-lg'>
+            <Text className='text-2xl'>{chapter.title}</Text>
+          </View>
+        </TouchableOpacity>
+      ))}
     </View>
-  )
+  );
 }
 
-export default home
-
-const styles = StyleSheet.create({
-  lightGreen: {
-    backgroundColor: '#2a9d8f',
-  },
-  lightOrange: {
-    backgroundColor: '#e76f51',
-  },
-})
+export default Home;
--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -6,7 +6,7 @@ const HomeHeader = () => {
 
     return (
         <View>
-            <View className="flex-row items-center justify-between pt-12 px-4 py-2 rounded-b-lg bg-indigo-700">
+            <View className="flex-row items-center justify-between pt-10 px-6 py-4 rounded-b-lg bg-emerald-700">
                 <Text className="text-3xl font-bold text-white">Study App</Text>
             </View>
         </View >
